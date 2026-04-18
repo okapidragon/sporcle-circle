1264bcf786b4fceaf79c72486492b7a7d08ebee3
okapidragon <142844243+okapidragon@users.noreply.github.com>
2026-04-18 11:47:45 -0400

    Import os module for template directory path

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 import math
 import time
+import os
 
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app = Flask(__name__, template_folder=template_dir)
